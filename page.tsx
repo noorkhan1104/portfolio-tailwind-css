7ab9233741e47bd561f5a0c58673ce02cdc93d52
noorkhan1104 <164567175+noorkhan1104@users.noreply.github.com>
2024-11-10 23:10:58 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,18 @@
+import Hero from "@/components/Hero";
+// import Link from "next/link";
+import AboutPage from "@/components/About";
+import Contact from "@/components/Contact";
+
+// import Image from "next/image";
+// import Navbar from "@/components/Navbar";
+
+const Home = () => {
+  return (
+    <main>
+      <Hero />
+      <Contact />
+      <AboutPage />
+    </main>
+  );
+};
+export default Home;
